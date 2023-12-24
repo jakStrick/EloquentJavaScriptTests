d52cb8fcea8180ebaa917649d713c1f8069aa8fb
David <65638264+jakStrick@users.noreply.github.com>
2023-12-23 18:12:02 -0800

    Added CC checker

--- EloquentJavaScriptTests/Program.cs
+++ EloquentJavaScriptTests/Program.cs
@@ -3,12 +3,17 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+// This is a collection of small test functions I created
+// for either school assignments or from the book Eloquent JavaScript
+// 2020 - 2023
 
 namespace EloquentJavaScriptTests
 {
     internal static class Program
     {
-        private static string m_ccType;
+        private static string? m_ccType;
 
         // program's entry point
         private static void Main(string[] args)
@@ -26,7 +31,7 @@ namespace EloquentJavaScriptTests
 
             Console.WriteLine("Sum of the range = " + Sum(range));
 
-            CreditCardChecker();
+            CreditCardValidator();
         }
 
         //finds minimum between two numbers
@@ -50,6 +55,7 @@ namespace EloquentJavaScriptTests
                 return IsEvenRecursive(n - 2);
         }
 
+        //check if number is even with modulous
         public static bool IsEvenModulus(int n)
         {
             if (n % 2 == 0)
@@ -58,6 +64,7 @@ namespace EloquentJavaScriptTests
             return false;
         }
 
+        //Get a range from two numbers
         public static object GetRange(int start, int end)
         {
             int step = start < end ? 1 : -1;
@@ -102,9 +109,8 @@ namespace EloquentJavaScriptTests
             return total;
         }
 
-        //cc checker
-
-        public static void CreditCardChecker()
+        //cc validator
+        public static void CreditCardValidator()
         {
             do
             {
@@ -114,126 +120,134 @@ namespace EloquentJavaScriptTests
                 if (ccNum == null)
                     return;
 
-                ValidateCCNum(ccNum);
-                Console.WriteLine(m_ccType);
-                Console.WriteLine();
+                VerifyCCNum(ccNum);
             } while (true);
         }
 
-        //let's get started checking the cc numbers
-        private static void ValidateCCNum(string cc)
+        //check the cc numbers valid
+        private static void VerifyCCNum(string cc)
         {
-            ChkFraudulent(cc);
-        }
-
-        //checks to see if CC is valid or not
-        private static void ChkFraudulent(string cc)
-        {
-            int s = cc.Length;
-            long[] n = new long[s];
-            long[] firstSet = new long[s / 2];
-
-            for (int i = 0; i < s; i++)
+            if (CCNumValid(cc))
             {
-                n[i] = long.Parse(cc.Substring(i, 1)); //fill the array with the cc numbers
-            }
-
-            int cnt = 0;
-            int len = 0;
-            string stemp = "";
-            int itemp = 0;
-
-            for (int i = s - 2; i >= 0; i -= 2)
-            {
-                if (i > 0)
-                {
-                    firstSet[cnt] = n[i] * 2;
-                    len = firstSet[cnt].ToString().Length;
-
-                    if (len > 1)
-                    {
-                        stemp = firstSet[cnt].ToString();
-                        itemp += (int)n[i];
-
-                        for (int j = 0; j < len; j++)
-                        {
-                            itemp += int.Parse(stemp.Substring(j, 1));
-                        }
-                        len = 0;
-                    }
-                    else
-                    {
-                        itemp += (int)firstSet[cnt];
-                        itemp += (int)n[i];
-                    }
-                }
-                else
-                {
-                    if (i == 0)
-                        itemp += (int)n[i];
-                    //Console.WriteLine("Value of itemp last one - " + itemp);
-                    ///Console.WriteLine("Value of n[" + n[cnt] + "] i = 0");
-                }
-
-                cnt++;
-            }
-
-            stemp = itemp.ToString();
-            GetCCType(cc, stemp, n);
-        }
-
-        //sets CC Type or Invalid
-        private static void GetCCType(string a, string t, long[] n)
-        {
-            int size = a.Length;
-            int len = t.Length;
-
-            if (len > 1)
-                t = t.Substring(len - 1);
-
-            bool validSize = size >= 13 && size <= 16;
-
-            //Console.WriteLine("Value of t inside GetCCType - " + t);
-
-            if (t == "0" && validSize)
-            {
-                string cc = n[0].ToString();
-
-                if (n[0] == 3 || n[0] == 5)
-                    cc += n[1].ToString();
-
-                //check valid size
-
-                switch (cc)
-                {
-                    case "34":
-                    case "37":
-
-                        m_ccType = "AMEX";
-                        break;
-
-                    case "4":
-
-                        m_ccType = "VISA";
-                        break;
-
-                    case "51":
-                    case "52":
-                    case "53":
-                    case "54":
-                    case "55":
-                        m_ccType = "MASTERCARD";
-                        break;
-
-                    default:
-                        m_ccType = "INVALID";
-                        break;
-                }
+                Console.Write("CC Number is valid. Type = ");
+                Console.WriteLine(m_ccType);
             }
             else
             {
-                m_ccType = "INVALID";
+                Console.Write("CC Number is not valid. Type = ");
             }
+
+            Console.WriteLine();
+        }
+
+        //checks to see if CC is valid or not
+        private static bool CCNumValid(string cc)
+        {
+            //remove any periods at the end
+            string trimmedCC = cc.Trim(new Char[] { '.' });
+
+            //get rid of the dashes
+            trimmedCC = Regex.Replace(trimmedCC, @"-", string.Empty);
+
+            //remove all white spaces
+            trimmedCC = Regex.Replace(trimmedCC, @"\s", string.Empty);
+
+            //remove any left over spaces at begining and end
+            trimmedCC = trimmedCC.Trim();
+
+            //if length isn't right or something isn't a digit reject it
+            if (trimmedCC.Length != 16 || !IsDigitsOnly(trimmedCC))
+                return false;
+
+            if (!CheckSum(trimmedCC))
+                return false;
+
+            string firstTwoNums = trimmedCC.Substring(0, 2);
+
+            if (firstTwoNums.Substring(0, 1) == "4")
+                firstTwoNums = "4";
+
+            return CCValid(firstTwoNums); ;
+        }
+
+        public static bool CheckSum(string cc)
+        {
+            int ccLen = cc.Length / 2;
+            int[] ccNum = new int[ccLen];
+            int[] ccNum1 = new int[ccLen];
+            //int[] ccNum2 = new int[ccLen / 2];
+
+            int step = 2;
+            int cnt = 0;
+            int checksum = 0;
+
+            for (int i = 0; i < ccLen; i++)
+            {
+                ccNum[i] = Int32.Parse(cc[cnt].ToString()) * 2;
+                ccNum1[i] = Int32.Parse(cc[cnt + 1].ToString());
+
+                Console.WriteLine("ccNum values " + ccNum[i]);
+                Console.WriteLine("ccNum1 values " + ccNum1[i]);
+
+                if (ccNum[i] >= 10)
+                {
+                    int num1 = ccNum[i] / 10;
+                    int num2 = ccNum[i] % 10;
+                    ccNum[i] = num1 + num2;
+                    Console.WriteLine("ccNum if >= 10 added values " + ccNum[i]);
+                }
+
+                checksum += ccNum[i] + ccNum1[i];
+                Console.WriteLine("checksum value " + checksum);
+                Console.WriteLine();
+
+                cnt += 2;
+            }
+
+            return checksum % 10 == 0;
+        }
+
+        //sets CC Type or Invalid
+        private static bool CCValid(string ccNums)
+        {
+            //check valid
+
+            switch (ccNums)
+            {
+                case "34":
+                case "37":
+
+                    m_ccType = "AMEX";
+                    return true;
+
+                case "4":
+
+                    m_ccType = "VISA";
+                    return true;
+
+                case "51":
+                case "52":
+                case "53":
+                case "54":
+                case "55":
+                    m_ccType = "MASTERCARD";
+                    return true;
+
+                default:
+                    m_ccType = "INVALID";
+                    return false;
+            }
+        }
+
+        public static bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+
+            return true;
         }
     }
 }
