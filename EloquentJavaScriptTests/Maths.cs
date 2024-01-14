9ed88beb8581c02cd3a65b6e176767fa84b309f5
David <65638264+jakStrick@users.noreply.github.com>
2024-01-13 17:45:03 -0800

    Add to its own class

---
+++ EloquentJavaScriptTests/Maths.cs
@@ -0,0 +1,77 @@
+﻿public static class Maths
+{
+    //finds minimum between two numbers
+    public static int Min(int n1, int n2)
+    {
+        if (n1 < n2)
+            return n1;
+        else
+            return n2;
+    }
+
+    //recursive call to find an even number
+    public static bool IsEvenRecursive(int n)
+    {
+        if (n == 0)
+            return true;
+
+        if (n < 0 || n == 1)
+            return false;
+        else
+            return IsEvenRecursive(n - 2);
+    }
+
+    //check if number is even with modulous
+    public static bool IsEvenModulus(int n)
+    {
+        if (n % 2 == 0)
+            return true;
+
+        return false;
+    }
+
+    //Get a range from two numbers
+    public static object GetRange(int start, int end)
+    {
+        int step = start < end ? 1 : -1;
+
+        //var array = new int[80];
+
+        int[] array = Array.Empty<int>();
+
+        int arraySize = start - end;
+
+        //turn it to positive
+        if (arraySize < 0)
+            arraySize *= -1;
+
+        Array.Resize(ref array, arraySize + 1);
+
+        if (step < 0)
+        {
+            for (int i = arraySize; i >= 0; i += step)
+            {
+                array[i] = start - i;
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= arraySize; i += step)
+            {
+                array[i] = start + i;
+            }
+        }
+
+        return array;
+    }
+
+    public static int Sum(int[] array)
+    {
+        var total = 0;
+        foreach (int value in array)
+        {
+            total += value;
+        }
+        return total;
+    }
+}
--- EloquentJavaScriptTests/Program.cs
+++ EloquentJavaScriptTests/Program.cs
@@ -1,4 +1,4 @@
-﻿// This is a collection of small test functions I created
+﻿// This is a collection of small functions I created
 // for either school assignments or from the book Eloquent JavaScript
 // 2020 - 2023
 
@@ -9,99 +9,24 @@ namespace EloquentJavaScriptTests
         // program's entry point
         private static void Main(string[] args)
         {
-            Console.WriteLine("The min value is " + Min(3, 1));
+            CCValidator.CreditCardValidator();
 
-            Console.WriteLine("The number is even " + IsEvenRecursive(2));
+            Console.WriteLine("The min value is " + Maths.Min(3, 1));
 
-            Console.WriteLine("The number is even " + IsEvenModulus(5));
+            Console.WriteLine("The number is even " + Maths.IsEvenRecursive(2));
 
-            int[] range = (int[])GetRange(10, 0);
+            Console.WriteLine("The number is even " + Maths.IsEvenModulus(5));
+
+            int[] range = (int[])Maths.GetRange(10, 0);
 
             for (int i = 0; i < range.Length; i++)
                 Console.WriteLine("All numbers in the range = " + range[i]);
 
-            Console.WriteLine("Sum of the range = " + Sum(range));
-
-            //CreditCardValidator();
+            Console.WriteLine("Sum of the range = " + Maths.Sum(range));
 
             //Transform matrixes with ChatGPT
             PixTo_mm();
             Transform_XYT_Offsets();
-        }
-
-        //finds minimum between two numbers
-        public static int Min(int n1, int n2)
-        {
-            if (n1 < n2)
-                return n1;
-            else
-                return n2;
-        }
-
-        //recursive call to find an even number
-        public static bool IsEvenRecursive(int n)
-        {
-            if (n == 0)
-                return true;
-
-            if (n < 0 || n == 1)
-                return false;
-            else
-                return IsEvenRecursive(n - 2);
-        }
-
-        //check if number is even with modulous
-        public static bool IsEvenModulus(int n)
-        {
-            if (n % 2 == 0)
-                return true;
-
-            return false;
-        }
-
-        //Get a range from two numbers
-        public static object GetRange(int start, int end)
-        {
-            int step = start < end ? 1 : -1;
-
-            //var array = new int[80];
-
-            int[] array = Array.Empty<int>();
-
-            int arraySize = start - end;
-
-            //turn it to positive
-            if (arraySize < 0)
-                arraySize *= -1;
-
-            Array.Resize(ref array, arraySize + 1);
-
-            if (step < 0)
-            {
-                for (int i = arraySize; i >= 0; i += step)
-                {
-                    array[i] = start - i;
-                }
-            }
-            else
-            {
-                for (int i = 0; i <= arraySize; i += step)
-                {
-                    array[i] = start + i;
-                }
-            }
-
-            return array;
-        }
-
-        public static int Sum(int[] array)
-        {
-            var total = 0;
-            foreach (int value in array)
-            {
-                total += value;
-            }
-            return total;
         }
 
         /// <summary>
